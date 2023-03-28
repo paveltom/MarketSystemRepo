bdd3a0916b6dbe2a6fc28bbbc5f6b12034f7fabe
Amihai Blekherov <amihaibl@post.bgu.ac.il>
2023-03-28 10:48:29 +0300

    Creating the Domain Layer with all the classes.

---
+++ Market_System/Domain Layer/Communication Component/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Market_System.Domain_Layer.Communication_Component
+{
+    public class Message : Notification
+    {
+    }
+}
---
+++ Market_System/Domain Layer/User Component/UserFacade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Market_System.Domain_Layer.User_Component
+{
+    //TODO:: Implement as a Singletone. 
+    public class UserFacade
+    {
+        
+    }
+}
