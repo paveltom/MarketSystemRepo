19f7381d574e3028ac7283338d0a75d9be512e08
bayan kassem <bayanka@post.bgu.ac.il>
2023-03-29 09:11:08 +0300

    added respone class

---
+++ Market_System/Service Layer/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroSE.Kanban.Backend.ServiceLayer
+{
+    ///<summary>Class <c>Response</c> represents the result of a call to a void function. 
+    ///If an exception was thrown, <c>ErrorOccured = true</c> and <c>ErrorMessage != null</c>. 
+    ///Otherwise, <c>ErrorOccured = false</c> and <c>ErrorMessage = null</c>.</summary>
+    public class Response
+    {
+        public readonly string ErrorMessage;
+        public bool ErrorOccured { get => ErrorMessage != null; }
+        internal Response() { }
+        internal Response(string msg)
+        {
+            this.ErrorMessage = msg;
+        }
+    }
+}
