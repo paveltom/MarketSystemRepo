26fd33c40d66adedb92cfdfd2f53c8a92c664b11
yotamdinar <dinaryo@post.bgu.ac.il>
2023-03-31 12:18:27 +0300

    inProgress

---
+++ Market_System/Domain Layer/DeliveyProxy/DeliveryInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_System.Domain_Layer.DeliveyProxy
+{
+    internal interface Interface1
+    {
+    }
+}
---
+++ Market_System/Domain Layer/PaymentProxy/PayCashService_dummy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_System.Domain_Layer.PaymentProxy
+{
+    public class PayCashService_dummy : PaymentInterface
+    {
+
+        public PayCashService_dummy() { }
+
+        public bool pay(double amount)
+        {
+            if (amount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
