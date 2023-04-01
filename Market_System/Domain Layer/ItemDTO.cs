e69317e7be505166aeaffdbcad65f0fd6e3acdc1
michelmo <80560001+michel1999m@users.noreply.github.com>
2023-04-01 15:29:51 +0300

    add fields for itemDTO and product class

--- Market_System/Domain Layer/ItemDTO.cs
+++ Market_System/Domain Layer/ItemDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Market_System.Domain_Layer.Store_Component;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,22 @@ namespace Market_System.Domain_Layer
 {
     public class ItemDTO
     {
+        private string _catagory;
+        private string _description;
+        private double _price;
+        private string _name;
+        private int _itemId;
+
+    public ItemDTO(Product product)
+        {
+            _description = product.GetDescription();
+            _catagory = product.GetCatagory();
+            _itemId = product.GetProductID();
+            _name = product.GetName();
+            _price = product.GetPrice();
+        }
+
+
+
     }
 }
