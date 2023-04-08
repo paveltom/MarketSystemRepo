c03d764c2eef0f177d7ee33f971b5e90a9ce20cd
paveltom <paveltom@post.bgu.ac.il>
2023-04-08 13:40:01 +0300

    namespace fixed: storecontroller, storefacade, store, product, storerepo, category, employeesclasses

--- Market_System/DomainLayer/StoreComponent/Property.cs
+++ Market_System/DomainLayer/StoreComponent/Property.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Market_System.Domain_Layer.Store_Component
+namespace Market_System.DomainLayer.StoreComponent
 {
     interface Property
     { 
