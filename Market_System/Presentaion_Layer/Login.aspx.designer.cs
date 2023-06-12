1990e2fdacdbf38f3562273a785ed945c3eec3e3
michelmo <80560001+michel1999m@users.noreply.github.com>
2023-06-12 12:01:29 +0300

    updating register page

--- Market_System/Presentaion_Layer/Login.aspx.designer.cs
+++ Market_System/Presentaion_Layer/Login.aspx.designer.cs
@@ -15,22 +15,13 @@ namespace Market_System.Presentaion_Layer
     {
 
         /// <summary>
-        /// Image1 control.
+        /// RequiredFieldValidator1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
-
-        /// <summary>
-        /// user_name_validator control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator user_name_validator;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
 
         /// <summary>
         /// txt_username control.
--- Market_System/Presentaion_Layer/register_page.aspx.designer.cs
+++ Market_System/Presentaion_Layer/register_page.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace Market_System.Presentaion_Layer
     {
 
         /// <summary>
-        /// Image1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
-
-        /// <summary>
         /// txt_username control.
         /// </summary>
         /// <remarks>
